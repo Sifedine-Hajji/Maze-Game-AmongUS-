521c09813b750060e6acc15db607b7f611b1ae0e
Sifedine <Sifedinehajji01@gmail.com>
2021-03-23 23:16:39 +0100

    readme link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Amazeing Maze Project
 
-## What ? 
+## What ?
 
 The work consists in the creation of a Maze in javascript by following the steps of the repository: [becodeorg/bxl-hopper-1-25](https://github.com/becodeorg/bxl-hopper-1-25/tree/master/The%20Hill/projects/2.amazeing).
 
@@ -10,29 +10,27 @@ The work was given on Monday 23th November and i finished it on wednesday 25th.
 
 ## **Functionalities :**
 
-* creation of a first level maze.
-* creation of a player and a ennemy.
-* creation of a 'treasure'.
-* animation when you die and geta treasure.
+- creation of a first level maze.
+- creation of a player and a ennemy.
+- creation of a 'treasure'.
+- animation when you die and geta treasure.
 
 ### **Functionalities that need improvements :**
 
-* animations doesn't work when i open it with githubPage.
+- animations doesn't work when i open it with githubPage.
 
 ## What does it look like ?
 
-Here is the direct link to the project i made [Among Us MAZE](https://sifedine-hajji.github.io/amazeing/).
-
+Here is the direct link to the project i made [Among Us MAZE](https://sifedine-hajji.github.io/Maze-Game-AmongUS-/.).
 
 ### **What i want to add later**
 
-* ennemy can follow us.
-* generate 5 more levels.
+- ennemy can follow us.
+- generate 5 more levels.
 
 #### project screenshot :
 
 <img src="Pics/Screenshot.PNG" alt="drawing" width="700"/>
-
 
 ## How I Proceeded ?
 
--- maze.js
+++ maze.js
@@ -1,8 +1,7 @@
-document.querySelector('.main').innerHTML = '';
-const main = document.querySelector('.main');
+document.querySelector(".main").innerHTML = "";
+const main = document.querySelector(".main");
 
-const multiline =
-`
+const multiline = `
 ***********.*
 *S.....**.*.T
 *****.....*.*
@@ -15,7 +14,7 @@ const multiline =
 *.******....*
 *.......**.**
 *************
-`
+`;
 
 //========================= REGLES ET DESCRIPTION =======================//
 
@@ -24,157 +23,176 @@ const multiline =
 
 //========================== JUMBOTRON ========================//
 
-const jumbotron = document.createElement('div');
+const jumbotron = document.createElement("div");
 jumbotron.className = "jumbotron";
 main.appendChild(jumbotron);
 
-const image1 = document.createElement('img');
-image1.src = 'Pics/jumbotron2.png';
+const image1 = document.createElement("img");
+image1.src = "Pics/jumbotron2.png";
 jumbotron.appendChild(image1);
 
-const titre = document.createElement('h1');
-titre.textContent = 'WELCOME TO THE AMONG US MAZE GAME !';
+const titre = document.createElement("h1");
+titre.textContent = "WELCOME TO THE AMONG US MAZE GAME !";
 jumbotron.appendChild(titre);
 
-const image2 = document.createElement('img');
-image2.src = 'Pics/jumbotron2.png';
+const image2 = document.createElement("img");
+image2.src = "Pics/jumbotron2.png";
 jumbotron.appendChild(image2);
-
-
-
 
 //============================ MAIN CONTENU===============================//
 
-
-const labyrinthe = document.createElement('div')
+const labyrinthe = document.createElement("div");
 labyrinthe.className = "labyrinthe";
 main.appendChild(labyrinthe);
 
-const player = document.createElement('div');
-player.className = 'joueur';
+const player = document.createElement("div");
+player.className = "joueur";
 
-const imposteur = document.createElement('div')
-imposteur.className = 'imposteur';
+const imposteur = document.createElement("div");
+imposteur.className = "imposteur";
 
 //========================= creation du labyrinthe ======================//
 
 for (let i = 0; i < multiline.length; i++) {
-    
-    if (multiline[i].split('') == '*') {
+  if (multiline[i].split("") == "*") {
+    const walls = document.createElement("div");
+    walls.className = "murs tile";
+    labyrinthe.appendChild(walls);
+  } else if (multiline[i].split("") == ".") {
+    const paths = document.createElement("div");
+    paths.className = "chemins tile";
+    labyrinthe.appendChild(paths);
+  } else if (multiline[i].split("") == "S") {
+    const start = document.createElement("div");
+    start.className = "chemins start tile ";
+    labyrinthe.appendChild(start);
+  } else if (multiline[i].split("") == "T") {
+    const end = document.createElement("div");
+    end.className = "chemins end tile ";
+    labyrinthe.appendChild(end);
 
-        const walls = document.createElement('div');
-        walls.className = 'murs tile';
-        labyrinthe.appendChild(walls);
-    } else if (multiline[i].split('') == '.') {
+    const treasure = document.createElement("div");
+    treasure.className = "tresor";
+    end.appendChild(treasure);
+  } else if (multiline[i].split("") == "I") {
+    const deadcase = document.createElement("div");
+    deadcase.className = "chemins tile";
+    labyrinthe.appendChild(deadcase);
 
-        const paths = document.createElement('div');
-        paths.className = 'chemins tile';
-        labyrinthe.appendChild(paths);
-
-    } else if (multiline[i].split('') == 'S') {
-
-        const start = document.createElement('div');
-        start.className = 'chemins start tile ';
-        labyrinthe.appendChild(start);
-
-    } else if (multiline[i].split('') == 'T') {
-
-        const end = document.createElement('div');
-        end.className = 'chemins end tile ';
-        labyrinthe.appendChild(end);
-
-        const treasure = document.createElement('div');
-        treasure.className = 'tresor';
-        end.appendChild(treasure);
-
-    }else if (multiline[i].split('') == 'I') {
-        const deadcase = document.createElement('div');
-        deadcase.className = 'chemins tile';
-        labyrinthe.appendChild(deadcase);
-
-        console.log(imposteur);
-    }
-    
-
+    console.log(imposteur);
+  }
 }
 
 //====================== DEPLACEMENTS JOUEURS ========================//
 
-    //==================== FONCTIONS ======================//
-    function movetop() {
-    let play = playerpos - 13
-    if (document.querySelector("body > main > div div:nth-child(" + play + ")").classList.contains('chemins')) {
-        playerpos = play
+//==================== FONCTIONS ======================//
+function movetop() {
+  let play = playerpos - 13;
+  if (
+    document
+      .querySelector("body > main > div div:nth-child(" + play + ")")
+      .classList.contains("chemins")
+  ) {
+    playerpos = play;
 
-        document.querySelector("body > main > div div:nth-child(" + playerpos + ")").appendChild(player)
-    } else {
-        console.log('il y a un mur');
-    }
+    document
+      .querySelector("body > main > div div:nth-child(" + playerpos + ")")
+      .appendChild(player);
+  } else {
+    console.log("il y a un mur");
+  }
 }
 
 function moveright() {
-    let play = playerpos + 1
-    if (document.querySelector("body > main > div div:nth-child(" + play + ")").classList.contains('chemins')) {
-        playerpos++
+  let play = playerpos + 1;
+  if (
+    document
+      .querySelector("body > main > div div:nth-child(" + play + ")")
+      .classList.contains("chemins")
+  ) {
+    playerpos++;
 
-        document.querySelector("body > main > div div:nth-child(" + playerpos + ")").appendChild(player)
-    } else {
-        console.log('il y a un mur');
-    }
+    document
+      .querySelector("body > main > div div:nth-child(" + playerpos + ")")
+      .appendChild(player);
+  } else {
+    console.log("il y a un mur");
+  }
 }
 
 function moveleft() {
-    let play = playerpos - 1
-    if (document.querySelector("body > main > div div:nth-child(" + play + ")").classList.contains('chemins')) {
-        playerpos--
+  let play = playerpos - 1;
+  if (
+    document
+      .querySelector("body > main > div div:nth-child(" + play + ")")
+      .classList.contains("chemins")
+  ) {
+    playerpos--;
 
-        document.querySelector("body > main > div div:nth-child(" + playerpos + ")").appendChild(player)
-    } else {
-        console.log('il y a un mur');
-    }
+    document
+      .querySelector("body > main > div div:nth-child(" + playerpos + ")")
+      .appendChild(player);
+  } else {
+    console.log("il y a un mur");
+  }
 }
 
 function movebottom() {
-    let play = playerpos + 13
-    if (document.querySelector("body > main > div div:nth-child(" + play + ")").classList.contains('chemins')) {
-        playerpos = playerpos + 13
+  let play = playerpos + 13;
+  if (
+    document
+      .querySelector("body > main > div div:nth-child(" + play + ")")
+      .classList.contains("chemins")
+  ) {
+    playerpos = playerpos + 13;
 
-        document.querySelector("body > main > div div:nth-child(" + playerpos + ")").appendChild(player)
-    } else {
-        console.log('il y a un mur');
-    }
+    document
+      .querySelector("body > main > div div:nth-child(" + playerpos + ")")
+      .appendChild(player);
+  } else {
+    console.log("il y a un mur");
+  }
 }
 
 //======================== players positions ========================//
-document.querySelector("body > main > div > div.start").appendChild(player)
-let playerpos = 15
-document.querySelector("body > main > div > div:nth-child(49)").appendChild(imposteur)
-let impostorpos = 49
+document.querySelector("body > main > div > div.start").appendChild(player);
+let playerpos = 15;
+document
+  .querySelector("body > main > div > div:nth-child(49)")
+  .appendChild(imposteur);
+let impostorpos = 49;
 //========================= Conditions mouvements ======================//
 
-document.body.addEventListener('keyup', function (e) {
-
-    if (e.key == 'ArrowUp') {
-        movetop()
-    }
-    if (e.key == 'ArrowRight') {
-        moveright()
-    }
-    if (e.key == 'ArrowLeft') {
-        moveleft()
-    }
-    if (e.key == 'ArrowDown') {
-        movebottom()
-    }
-    if (playerpos == 26 ) {
-    main.innerHTML = "<video autoplay muted><source src='vids/Emergency.mp4' type='video/mp4' /></video>"
-    setTimeout(function(){ document.querySelector("body > main > video").style.display = location.reload() }, 2000);
-    }
-    if (playerpos == impostorpos) {
-        main.innerHTML = "<video autoplay muted><source src='vids/red-kill-white.mp4' type='video/mp4' /></video>"
-        setTimeout(function(){ document.querySelector("body > main > video").style.display = location.reload() }, 5000);
-        alert('you are dead');
-    }
-
-
-})
+document.body.addEventListener("keyup", function (e) {
+  if (e.key == "ArrowUp") {
+    movetop();
+  }
+  if (e.key == "ArrowRight") {
+    moveright();
+  }
+  if (e.key == "ArrowLeft") {
+    moveleft();
+  }
+  if (e.key == "ArrowDown") {
+    movebottom();
+  }
+  if (playerpos == 26) {
+    main.innerHTML =
+      "<video autoplay muted><source src='./vids/Emergency.mp4' type='video/mp4' /></video>";
+    setTimeout(function () {
+      document.querySelector(
+        "body > main > video"
+      ).style.display = location.reload();
+    }, 2000);
+  }
+  if (playerpos == impostorpos) {
+    main.innerHTML =
+      "<video autoplay muted><source src='./vids/red-kill-white.mp4' type='video/mp4' /></video>";
+    setTimeout(function () {
+      document.querySelector(
+        "body > main > video"
+      ).style.display = location.reload();
+    }, 5000);
+    alert("you are dead");
+  }
+});
